d67a8b89bf5cfbb90254e19da577d178040d2da3
DavGonzaj <davgonzaj@gmail.com>
2023-06-05 10:48:36 -0700

    Final Updates on Fizz

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,8 +1,44 @@
-﻿void FizzBuzz()
+﻿void OneToTwoFityFive()
+{
+    for (int i = 1; i <= 255; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+OneToTwoFityFive();
+
+
+
+
+
+
+
+
+
+void FizzBuzz()
 {
     for (int i = 1; i <= 100; i++)
     {
-        Console.WriteLine(i);
+        bool isDivisibleBy3 = i % 3 == 0;
+        bool isDivisibleBy5 = i % 5 == 0;
+        bool isDivisibleBy3And5 = isDivisibleBy3 && isDivisibleBy5;
+        if (isDivisibleBy3And5)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (isDivisibleBy3)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (isDivisibleBy5)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
     }
 }
 
