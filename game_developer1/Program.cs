4014f85d7935b130c3988d28b1fbb05ec4dad6fa
DavGonzaj <davgonzaj@gmail.com>
2023-06-06 22:28:38 -0700

    Separate

--- game_developer1/Program.cs
+++ game_developer1/Program.cs
@@ -1,38 +1,4 @@
-﻿class Attack
-{
-    public string Name { get; }
-    public int DamageAmount { get; }
-
-    public Attack(string name, int damageAmount)
-    {
-        Name = name;
-        DamageAmount = damageAmount;
-    }
-}
-
-class Enemy
-{
-    public string Name { get; set; }
-    public int Health { get; private set; }
-    public List<Attack> AttackList { get; }
-
-    public Enemy(string name)
-    {
-        Name = name;
-        Health = 100;
-        AttackList = new List<Attack>();
-    }
-
-    public void RandomAttack()
-    {
-        Random random = new Random();
-        int attackIndex = random.Next(AttackList.Count);
-        Attack randomAttack = AttackList[attackIndex];
-        Console.WriteLine($"The {Name} performs a {randomAttack.Name} attack, dealing {randomAttack.DamageAmount} damage.");
-    }
-}
-
-class Program
+﻿class Program
 {
     static void Main()
     {
