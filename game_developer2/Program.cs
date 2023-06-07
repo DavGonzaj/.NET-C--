428af07023efa7f42251cab4998535697b3c1794
DavGonzaj <davgonzaj@gmail.com>
2023-06-07 09:16:10 -0700

    updates

--- game_developer2/Program.cs
+++ game_developer2/Program.cs
@@ -1,56 +1,60 @@
-﻿class RangedFighter : Enemy
+﻿class Program
 {
-    public int Distance { get; private set; }
-
-    public RangedFighter() : base("Ranged Fighter")
+    static void Main() { }
+    class RangedFighter : Enemy
     {
-        Health = 100;
-        Distance = 5;
+        public int Distance { get; private set; }
 
-        Attack shootArrow = new Attack("Shoot an Arrow", 20);
-        Attack throwKnife = new Attack("Throw a Knife", 15);
-
-        AttackList.Add(shootArrow);
-        AttackList.Add(throwKnife);
-    }
-
-    public void Dash()
-    {
-        Distance = 20;
-        Console.WriteLine($"{Name} performs a dash. Distance is now {Distance}.");
-    }
-
-    public void PerformAttack(Enemy target, Attack chosenAttack)
-    {
-        if (Distance < 10)
+        public RangedFighter() : base("Ranged Fighter")
         {
-            Console.WriteLine($"{Name} is too close to perform a ranged attack.");
-            return;
+            Health = 100;
+            Distance = 5;
+
+            Attack shootArrow = new Attack("Shoot an Arrow", 20);
+            Attack throwKnife = new Attack("Throw a Knife", 15);
+
+            AttackList.Add(shootArrow);
+            AttackList.Add(throwKnife);
         }
 
-        base.PerformAttack(target, chosenAttack);
-    }
-}
+        public void Dash()
+        {
+            Distance = 20;
+            Console.WriteLine($"{Name} performs a dash. Distance is now {Distance}.");
+        }
 
-class MagicCaster : Enemy
-{
-    public MagicCaster() : base("Magic Caster")
+        public void PerformAttack(Enemy target, Attack chosenAttack)
+        {
+            if (Distance < 10)
+            {
+                Console.WriteLine($"{Name} is too close to perform a ranged attack.");
+                return;
+            }
+
+            base.PerformAttack(target, chosenAttack);
+        }
+    }
+
+    class MagicCaster : Enemy
     {
-        Health = 80;
+        public MagicCaster() : base("Magic Caster")
+        {
+            Health = 80;
 
-        Attack fireball = new Attack("Fireball", 25);
-        Attack lightningBolt = new Attack("Lightning Bolt", 20);
-        Attack staffStrike = new Attack("Staff Strike", 10);
+            Attack fireball = new Attack("Fireball", 25);
+            Attack lightningBolt = new Attack("Lightning Bolt", 20);
+            Attack staffStrike = new Attack("Staff Strike", 10);
 
-        AttackList.Add(fireball);
-        AttackList.Add(lightningBolt);
-        AttackList.Add(staffStrike);
+            AttackList.Add(fireball);
+            AttackList.Add(lightningBolt);
+            AttackList.Add(staffStrike);
+        }
+
+        public void Heal(Enemy target)
+        {
+            target.Health += 40;
+            Console.WriteLine($"{Name} heals {target.Name} for 40 health. {target.Name}'s health is now {target.Health}.");
+        }
+
     }
-
-    public void Heal(Enemy target)
-    {
-        target.Health += 40;
-        Console.WriteLine($"{Name} heals {target.Name} for 40 health. {target.Name}'s health is now {target.Health}.");
-    }
-
 }
