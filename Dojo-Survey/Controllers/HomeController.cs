e38098f0ef72b20c76f2000861be54c222c8934f
DavGonzaj <davgonzaj@gmail.com>
2023-06-08 00:43:36 -0700

    Dojo survey finished

---
+++ Dojo-Survey/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+
+    [HttpGet("")]
+    public ActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost("result")]
+    public ActionResult Result(string name, string location, string language, string textarea)
+    {
+        ViewBag.name = name;
+        ViewBag.location = location;
+        ViewBag.language = language;
+        ViewBag.textarea = textarea;
+
+        return View();
+    }
+
+    //Redirect back to Index if trying to access result page through GET
+    [HttpGet("result")]
+    public RedirectToActionResult Redirect()
+    {
+        return RedirectToAction("Index");
+    }
+}
